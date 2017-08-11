ae1abf6d00ec7407418e0e0db250f6a5aa6454d8
Alex Masterov <alex.masterow@gmail.com>
2017-08-11 13:16:39 +0300

    [Computation] Add the decorate session trait to PureClient

--- src/Computation/PureClient.php
+++ src/Computation/PureClient.php
@@ -3,17 +3,17 @@ declare(strict_types=1);
 
 namespace Tarantool\Client;
 
-use Tarantool\{
-    Client,
-    Client\Command,
-    Client\Response,
-    Computation\Result
+use Tarantool\Client;
+use Tarantool\Client\{
+    Command,
+    Response,
+    Session\CanDecoratedSession
 };
+use Tarantool\Computation\Result;
 
 final class PureClient implements Client
 {
-    /** @var Client */
-    private $decoratedClient;
+    use CanDecoratedSession;
 
     public function __construct(Client $client)
     {

018ef1822457d9e9cc603e88dafbea1e7da49f3f
Alex Masterov <alex.masterow@gmail.com>
2017-08-18 07:10:07 +0300

    [Connector] Improve Connection exception

--- src/Connector/Connection/ConnectionException.php
+++ src/Connector/Connection/ConnectionException.php
@@ -8,28 +8,40 @@ use Tarantool\TarantoolException;
 
 final class ConnectionException extends RuntimeException implements TarantoolException
 {
-    public static function unableConnect(string $host, string $message, int $code): ConnectionException
-    {
-        return new static("Unable to connect to {$host}: {$message}", $code);
-    }
+    public const CANNOT_WRITE = 1;
+    public const UNABLE_READ_BYTES = 2;
+    public const TIMEOUT = 3;
+    public const CANNOT_COMMUNICATE = 4;
 
     public static function cannotWrite(): ConnectionException
     {
-        return new static('Cannot write to stream');
+        return new static(
+            'Cannot write to stream',
+            self::CANNOT_WRITE
+        );
     }
 
     public static function unableReadBytes(int $bytes): ConnectionException
     {
-        return new static("Unable to read data bytes ({$bytes}) from stream");
+        return new static(
+            "Unable to read data bytes ({$bytes}) from stream",
+            self::UNABLE_READ_BYTES
+        );
     }
 
     public static function timeout(): ConnectionException
     {
-        return new static('Connection has timed out');
+        return new static(
+            'Connection has timed out',
+            self::TIMEOUT
+        );
     }
 
     public static function cannotCommunicate(): ConnectionException
     {
-        return new static('Cannot communicate when there is no connection');
+        return new static(
+            'Cannot communicate when there is no connection',
+            self::CANNOT_COMMUNICATE
+        );
     }
 }

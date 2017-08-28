7790c6d26186f52ff7cbec47876f86ea7ae216ef
Alex Masterov <alex.masterow@gmail.com>
2017-08-28 06:05:05 +0300

    [Connector] Add interface for Sensor event emitter

---
+++ src/Connector/Sensor.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Tarantool\Connector;
+
+use Closure;
+
+interface Sensor
+{
+    public function on(string $event, Closure $listener): void;
+
+    public function off(string $event, Closure $listener): void;
+
+    public function emit(string $event, ...$arguments): void;
+}

6bab2a5d955414d63f15b4252afce66332f63324
Alex Masterov <alex.masterow@gmail.com>
2018-03-11 02:20:28 +0300

    [Protocol] Add new MessagePack serialization library

---
+++ src/Protocol/MessagePack/Standart.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Tarantool\Protocol\MessagePack;
+
+use MessagePack\{Decoder, Encoder};
+use Tarantool\Protocol\{
+    MessagePack,
+    Request
+};
+use function strlen;
+
+final class Standart implements MessagePack
+{
+    /** @var Encoder */
+    private $encoder;
+
+    /** @var Decoder */
+    private $decoder;
+
+    public function __construct(
+        Encoder $encoder = null,
+        Decoder $decoder = null
+    ) {
+        $this->encoder = $encoder ?? new Encoder();
+        $this->decoder = $decoder ?? new Decoder();
+    }
+
+    public function pack(Request $request): string
+    {
+        $data = $this->encoder->encodeMap($request->header());
+        if (!empty($request->body())) {
+            $data .= $this->encoder->encodeMap($request->body());
+        }
+
+        $length = $this->encoder->encodeInt(strlen($data));
+
+        return "${length}${data}";
+    }
+
+    public function unpack(string $data)
+    {
+        return $this->decoder->decode($data);
+    }
+}

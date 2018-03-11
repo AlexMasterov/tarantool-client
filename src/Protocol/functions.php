52fb34b64761130a24c8a4bd681aaea35f02dcec
Alex Masterov <alex.masterow@gmail.com>
2018-03-11 03:18:30 +0300

    [Protocol] Improve chap_sha1 function

--- src/Protocol/functions.php
+++ src/Protocol/functions.php
@@ -6,19 +6,18 @@ namespace Tarantool\Protocol;
 use function sha1;
 use function substr;
 
-/** @link https://tarantool.org/doc/1.7/dev_guide/internals_index.html#authentication */
+const SCRAMBLE_LENGTH = 20;
+
 function chap_sha1(string $password, string $salt): string
 {
-    static $scrambleLength = 20;
-
-    $salt = substr($salt, 0, $scrambleLength);
+    $salt = substr($salt, 0, SCRAMBLE_LENGTH);
 
     $hash1 = sha1($password, true);
     $hash2 = sha1($hash1, true);
-    $hash3 = sha1("{$salt}{$hash2}", true);
+    $hash3 = sha1("${salt}${hash2}", true);
 
     $scramble = '';
-    for ($i = 0; $i < $scrambleLength; ++$i) {
+    for ($i = 0; $i < SCRAMBLE_LENGTH; ++$i) {
         $scramble .= $hash1[$i] ^ $hash3[$i];
     }
 
